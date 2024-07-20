f31a783d679135fb91200e07d5eb6ed0831e11d5
Irfan Tariq <mr.irfan.tariq@gmail.com>
2024-07-20 15:24:08 +0800

    update

---
+++ forloop.py
@@ -0,0 +1,57 @@
+#generate random numbers:
+import random 
+#mynumber = random.randint(1, 1000)
+for i in range(10):
+    mynumber = random.randint(1, 1000)
+    print(mynumber)
+
+
+# loan = 1000
+# Apr = 0.05 
+# for i in range(10):
+#     loan+= (loan*Apr)
+#     print("Year", i+1, "is", round(loan, 2))
+# for i in range(100,111):
+#     print(i)
+# for i in range(1,8):
+#     print("Days", i)
+# print("Thirteen times table")
+# for i in range(1,11):
+#     print(i, "x 13 = " , i * 13)
+# for i in range(10,100,20):
+#     print(i)
+# for i in range(10,-1, -1):
+#     print(i)
+
+
+# for i in range (10, 0, -1):
+#     print(i)
+
+# for i in range (20, 40, 1):
+#   print(i)
+
+# x = int(input("please enter numer: "))
+# y = int(input("please enter the number: "))
+# z = int(input("please add the inrement you want: "))
+# for i in range(x, y, z):
+#     print(i)
+
+
+# user = int(input("please input the number you want multilipication: "))
+# count = 0
+# for i in range (1, 11):
+#     cor_ans = user * i 
+#     print (i , "x" , user )
+#     ans = int(input(">>>>>>>>>>>>>>>>>>>>"))
+#     if ans == cor_ans:
+#         print("you got it right")
+#         count += 1
+#     else:
+#         print("Thats not correct answer", cor_ans)  
+#     if count == 10:
+#         print("you got 10 out of 10 wow")
+#     else:
+#         print("you got ", count , "out of 10")          
+              
+
+
--- gussingnumber.py
+++ gussingnumber.py
@@ -1,4 +1,7 @@
-correct_number = 1500 
+import random
+my_number = random.randint(1, 1000)
+#correct_number = 1500 
+print(my_number)
 attempt = 0 
 
 while True:
@@ -6,17 +9,20 @@ while True:
     if user_guess <0:
         print("number is less then 0")
         exit()
-    if user_guess < correct_number:
+    if user_guess < my_number:
         print("the number is too low: ")
-        attempt +=1
-    elif user_guess > correct_number :
+        attempt += 1
+    elif user_guess > my_number :
         print("the number is too high: ")
-        attempt +=1 
-    if user_guess == correct_number:
+        attempt += 1 
+    if user_guess == my_number:
         print("you guess the correct number")
         break
     else:
-        print("this is not the number i recognized", attempt) 
+        print("You recognized the number in ", attempt) 
+
+
+
 
 
 
