70cdf92219ce6f06bbfb5b265985fa186169d0f4
Irfan Tariq <mr.irfan.tariq@gmail.com>
2024-07-20 16:29:23 +0800

    changed file

--- fun.py
+++ fun.py
@@ -10,16 +10,3 @@ def userlogin():
             continue  
         
 userlogin()        
-# def login():
-#   while True:
-#     username = input("What is your username?: ")
-#     password = input("What is your password? ")
-#     if username == "David" and password == "Replit4ev#r":
-#       print("Welcome David!")
-#       break
-#     else:
-#       print("That is not the correct username or password. Try again!")
-#       continue
-    
-# print("REPLIT LOGIN SYSTEM")
-# login()
